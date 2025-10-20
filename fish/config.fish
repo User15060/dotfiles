48d117efd8d48d0a9e5290170efb9f4a8b76c46d
User15060 <luc.del123@outlook.com>
2025-10-20 13:52:04 +0200

    kitty + fish

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias bat="batcat"
+alias open="xdg-open"
+
+# wifi
+alias wifi="nmtui"
+
+## eza
+alias ls="eza --icons"
+alias lla="eza --icons -la"
